e4c609f3145e3d79e56de8e658b3bf90cfb7db19
NikitaDenysenko <nik2012nik92@gmail.com>
2024-07-04 10:10:57 +0300

    add Article component

---
+++ src/components/Articles.tsx
@@ -0,0 +1,66 @@
+'use client'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import styles from './Articles.module.scss'
+
+const Articles = () => {
+    const [posts, setPosts] = useState([]);
+    const [total, setTotal] = useState(0);
+    const [limit, setLimit] = useState(10);
+    const [offset, setOffset] = useState(0);
+
+    const fetchPosts = async () => {
+        try {
+            const response = await axios.get('http://localhost:4000/posts', {
+                params: {
+                    limit,
+                    offset,
+                },
+            });
+            setPosts(response.data.posts);
+            setTotal(response.data.total);
+        } catch (error) {
+            console.error('Error fetching posts:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchPosts();
+    }, [limit, offset]);
+
+    const handleNextPage = () => {
+        if (offset + limit < total) {
+            setOffset(offset + limit);
+        }
+    };
+
+    const handlePreviousPage = () => {
+        if (offset - limit >= 0) {
+            setOffset(offset - limit);
+        }
+    };
+
+    return (
+        <div className={styles.postsContainer}>
+            <h1>Posts</h1>
+            <ul>
+                {posts.map((post) => (
+                    <a href={post.link} key={post.id} target="_blank">
+                        <li>{post.title}</li>
+                    </a>
+                ))}
+            </ul>
+            <div className={styles.pagination}>
+                <button onClick={handlePreviousPage} disabled={offset === 0}>
+                    Previous
+                </button>
+                <button onClick={handleNextPage} disabled={offset + limit >= total}>
+                    Next
+                </button>
+            </div>
+        </div>
+    );
+
+}
+
+export default Articles
